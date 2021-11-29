e8fa3b96326c4462d4537a414199968ebe19de50
Ismail Ahmed <isahmed0149@gmail.com>
2021-11-29 14:22:42 -0500

    Add Ticket entity class

---
+++ src/main/ticket.py
@@ -0,0 +1,10 @@
+class Ticket:
+
+    ticket_data: dict 
+
+    def __init__(self) -> None:
+        pass
+
+    def set_ticket(self, raw_data: dict) -> None:
+        self.ticket_data = raw_data 
+
